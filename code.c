ea3d7fc50b8ff69fbc31602bedc331273d5979a1
viraj <virajrakholiya14@gmail.com>
2021-12-08 21:17:12 +0530

    Initial commit

---
+++ code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+
+    int a;
+
+    printf("\n Enter The Value:");
+    scanf("%d", &a);
+
+    printf("\n Square of Your Value: %d", a * a);
+    
+
+    return 0;
+}
